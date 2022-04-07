6589d25fecf3afb58885f9d6ebfaa303cade13ac
Mical29 <randomlyh2020.mm@gmail.com>
2022-04-07 23:44:24 +0630

    add some config

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App()  {
 
           {/* Need to add page for / route */}
           
-          <Route path="/" name="Login Page" render={props => <Login {...props}/>} />
+          <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
           
         </Switch>
       
@@ -68,12 +68,12 @@ function App()  {
         <Router>
           
             <Switch>
-              <Route exact path="/" name="Home" render={props => <TheLayout data={artistDetail}/>} />
+              
               {/* <Route  path="/login" name="Login Page" render={props => <Login {...props}/>} />
               <Route  path="/register" name="Register Page" render={props => <Register {...props}/>} /> */}
-              <Route  path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
-              <Route  path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
-              
+              <Route  exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
+              <Route  exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
+              <Route path="/" name="Home" render={props => <TheLayout data={artistDetail}/>} />
             </Switch>
          
       </Router>
